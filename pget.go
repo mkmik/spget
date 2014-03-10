fc6af8afcdc4b7859d5747dfa6cbc19a86348afe
Marko Mikulicic <mmikulicic@gmail.com>
2014-03-10 17:11:53 +0000

    Cleanup and refactor; added -o flag.

--- pget.go
+++ pget.go
@@ -13,6 +13,7 @@ import (
 )
 
 var (
+	output    = flag.String("o", "-", "output file")
 	workers   = flag.Int("n", 4, "parallel connections")
 	chunkSize = flag.Int("c", 100*1024, "chunk size")
 	debug     = flag.Bool("d", false, "debug")
@@ -25,12 +26,10 @@ type chunk struct {
 }
 
 type download struct {
-	wg          sync.WaitGroup
-	url         string
-	readyChunks map[int64]*chunk
-	outPos      int64
-	size        int64
-	writer      io.Writer
+	wg     sync.WaitGroup
+	url    string
+	size   int64
+	writer io.Writer
 }
 
 func newDownload(url string) (*download, error) {
@@ -40,13 +39,15 @@ func newDownload(url string) (*download, error) {
 	}
 
 	return &download{
-		url:         url,
-		readyChunks: make(map[int64]*chunk),
-		size:        size,
-		writer:      os.Stdout,
+		url:    url,
+		size:   size,
+		writer: os.Stdout,
 	}, nil
 }
 
+// chunkFeeder generates chunk structures and sends them to the out channel.
+// The chunk size is determined via commandline params. The last chunk is
+// is sized accordingly.
 func (d *download) chunkFeeder(out chan<- *chunk) {
 	if *debug {
 		log.Printf("Feeder is running")
@@ -69,11 +70,14 @@ func (d *download) chunkFeeder(out chan<- *chunk) {
 	close(out)
 }
 
+// chunkWorker task chunks from the in channel, fetches the chunk from
+// the http resource into the buffer in the chunk structure.
+// The completed chunk is then sent to the out channel.
+// Any error causes to panic.
 func (d *download) chunkWorker(n int, in <-chan *chunk, out chan<- *chunk) {
 	if *debug {
 		log.Println("Started chunk worker", n)
 	}
-	defer d.wg.Done()
 
 	for ch := range in {
 		if *debug {
@@ -95,17 +99,24 @@ func (d *download) chunkWorker(n int, in <-chan *chunk, out chan<- *chunk) {
 	}
 }
 
+// chunkWriter reads completed chunks from the in channel,
+// and writes them out in the correct order.
+// Out of order chunks are kept in memory until the missing pieces
+// are delivered.
 func (d *download) chunkWriter(in <-chan *chunk) {
+	readyChunks := make(map[int64]*chunk)
+	outPos := int64(0)
+
 	for ch := range in {
 		if *debug {
 			log.Printf("Chunk writer processing completed chunk %d (%d)\n", ch.offset, len(ch.buffer.Bytes()))
 		}
-		d.readyChunks[ch.offset] = ch
+		readyChunks[ch.offset] = ch
 
 		for {
-			if c, ok := d.readyChunks[d.outPos]; ok {
+			if c, ok := readyChunks[outPos]; ok {
 				io.Copy(d.writer, &c.buffer)
-				d.outPos += c.size
+				outPos += c.size
 			} else {
 				break
 			}
@@ -113,12 +124,22 @@ func (d *download) chunkWriter(in <-chan *chunk) {
 	}
 }
 
+// start
 func start() error {
 	url := flag.Arg(0)
 
 	d, err := newDownload(url)
 	if err != nil {
 		return err
+	}
+
+	if *output != "-" {
+		f, err := os.Create(*output)
+		if err != nil {
+			return err
+		}
+		defer f.Close()
+		d.writer = f
 	}
 
 	if *debug {
@@ -128,10 +149,17 @@ func start() error {
 	ich := make(chan *chunk, 2)
 	och := make(chan *chunk, 2)
 
+	// spawn the chunk feeder.
 	go d.chunkFeeder(ich)
+
+	// spawn the workers in such a way
+	// that we know when they are all done.
 	for i := 0; i < *workers; i++ {
 		d.wg.Add(1)
-		go d.chunkWorker(i, ich, och)
+		go func(i int) {
+			d.chunkWorker(i, ich, och)
+			d.wg.Done()
+		}(i)
 	}
 
 	// when all the workers are gone
@@ -141,12 +169,15 @@ func start() error {
 		close(och)
 	}()
 
+	// the chunk writer block until
+	// all the chunks are received, reordered and written.
 	d.chunkWriter(och)
-	log.Printf("Chunk writer finished")
 
 	return nil
 }
 
+// contentSize returns the size of the resource
+// at the given url.
 func contentSize(url string) (int64, error) {
 	resp, err := http.Get(url)
 	if err != nil {
@@ -155,6 +186,16 @@ func contentSize(url string) (int64, error) {
 	return resp.ContentLength, nil
 }
 
+// followRedirect ensures that all headers are passed through the redirect.
+// This is necessary for the range header to work across redirects.
+func followRedirect(req *http.Request, via []*http.Request) error {
+	req.Header = via[0].Header
+	return nil
+}
+
+// fetch returns a ReadCloser with the content of a
+// section of the resource found at url starting at from
+// and len long.
 func fetch(url string, from, len int64) (io.ReadCloser, error) {
 	req, err := http.NewRequest("GET", url, nil)
 	if err != nil {
@@ -163,7 +204,9 @@ func fetch(url string, from, len int64) (io.ReadCloser, error) {
 
 	req.Header.Add("Range", fmt.Sprintf("bytes=%d-%d", from, from+len-1))
 
-	client := &http.Client{}
+	client := &http.Client{
+		CheckRedirect: followRedirect,
+	}
 	resp, err := client.Do(req)
 	if err != nil {
 		return nil, err
